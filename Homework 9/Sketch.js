2e9d33e61cbee93ae95bd3810b2e4fb7820cbfcf
cainfritz <70179804+cainfritz@users.noreply.github.com>
2020-10-16 21:22:54 -0600

    Add files via upload

---
+++ Homework 9/Sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(360, 100, 300);
+  ellipse(200,200,260,300);
+    ellipse(145,130,25,60);
+    ellipse(240,130,25,60);
+    triangle(175, 220, 203, 155, 231, 220);
+    ellipse(180,266,80,40);
+    line(30, 20, 85, 75);
+    line(370, 20, 315, 75);
+    textSize(28);
+      text('"Casual Onlooker"', 10, 375);
+      textSize(12);
+      text('By Cain Fritz', 15, 389);
+    
+  }
